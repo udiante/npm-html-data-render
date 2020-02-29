3d54be587d5b6fc1d82a3fac6ef1c2431609ce28
Alejandro Quibus <udiante@gmail.com>
2020-02-29 22:46:52 +0100

    JSON render

--- .vscode/test/Example-TestFile.js
+++ .vscode/test/Example-TestFile.js
@@ -5,23 +5,36 @@ const MD_PATH = __dirname + '/examples/md.md'
 const MD_INDEX = fs.readFileSync(MD_PATH, 'utf8').toString()
 const outputPath = __dirname + '/examples/output/'
 
-if (!fs.existsSync(outputPath)){
+if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath);
 }
 
-function test() {
-    
+function testMarkdown() {
+
     let htmlMD = Render.renderMarkdown(MD_INDEX)
     writeFile('fileMD.html', htmlMD)
     writeFile('serialFileMDLoad.html', Render.renderMarkDownFile(MD_PATH))
 
-    Render.renderMarkDownFile(MD_PATH, function(html){
+    Render.renderMarkDownFile(MD_PATH, function (html) {
         writeFile('asyncFileMDLoad.html', html)
     })
 }
 
 function writeFile(fileName, data) {
-    fs.writeFileSync(outputPath +fileName, data, { mode: 0o755 });
+    fs.writeFileSync(outputPath + fileName, data, { mode: 0o755 });
 }
 
-test()
+const JSON_PATH = __dirname + '/examples/json.json'
+
+function testJSON() {
+    writeFile('fileJSONObject.html', Render.renderJSON({ a: "a", 1: false }))
+    writeFile('fileJSONString.html', Render.renderJSON({ a: "a", 1: false }))
+    writeFile('serialFileJSONLoad.html', Render.renderJSONFile(JSON_PATH))
+
+    Render.renderMarkDownFile(JSON_PATH, function (html) {
+        writeFile('asyncFileJSONLoad.html', html)
+    })
+}
+
+// testMarkdown()
+testJSON()
--- index.js
+++ index.js
@@ -13,3 +13,17 @@ module.exports.renderMarkdown = require('./src/MarkdownRender').renderMarkdown
  * @returns {String} ***undefined** if a completion callback is provided
  */
 module.exports.renderMarkDownFile = require('./src/MarkdownRender').renderMarkDownFile
+
+/**
+ * Renders a JSON object into a out of the box HTML
+ * @returns {String}
+ */
+module.exports.renderJSON = require('./src/HTMLRender').renderJSON
+
+/**
+ * Renders a JSON object into a out of the box HTML
+ * @param {String} filePath Path where the file is located
+ * @param {Object} completion ***Optional** If provided the file load is async
+ * @returns {String} ***undefined** if a completion callback is provided
+ */
+module.exports.renderJSONFile = require('./src/HTMLRender').renderJSONFile
